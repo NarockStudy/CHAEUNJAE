695f51b48c4c37cab46ad143a547128970310990
ckdmswo17 <ckdmswo17@naver.com>
2025-10-02 15:14:46 +0900

    먹을 것인가 먹힐 것인가/실버3/메모리 2408KB/시간 32ms

---
+++ 7week/BOJ7795.cpp
@@ -0,0 +1,42 @@
+// 백준 문제 : 먹을 것인가 먹힐 것인가
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    for(int i=0;i<T;i++){
+        int N,M;
+        cin >> N >> M;
+        vector<int> A;
+        vector<int> B;
+
+        for(int j=0;j<N;j++){
+            int e;
+            cin >> e;
+            A.push_back(e);
+        }
+        for(int j=0;j<M;j++){
+            int e;
+            cin >> e;
+            B.push_back(e);
+        }
+
+        // 정렬 + 이분 탐색 버전
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        int count = 0;
+        for(int i=0;i<M;i++){
+            int ub = A.end()-upper_bound(A.begin(),A.end(),B[i]);
+            count += ub;
+        }
+        cout << count << "\n";
+    }
+}
